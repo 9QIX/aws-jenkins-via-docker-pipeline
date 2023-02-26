2433b948acc70276e75540c70ed2c9af0911030f
9QIX <klbulaong@student.hau.edu.ph>
2023-02-26 23:22:36 +0800

    jenkins and docker files

---
+++ jenkins/notif.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sendToSlack() {
+    # curl -X POST -H 'Content-type: application/json' --data '{"text": "Sample: Success"}'  https://hooks.slack.com/services/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
+    
+}
+
+sendToSlack
